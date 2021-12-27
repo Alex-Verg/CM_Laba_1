02b6527fd15c8c625ed8e47b0af456f869bf3767
Alex-Verg <alexvergelyuk@gmail.com>
2021-12-27 15:24:41 +0200

    Add to function parametrs to execute without print.

--- polynom.py
+++ polynom.py
@@ -37,8 +37,8 @@ class Polynom:
 
         return ddf
 
-    def bisection_method(self, a, b, epsilon):
-        print('-'*70 + '\n' + ' ' * 27 + 'Bisection method' + ' ' * 27 + '\n' + '-'*70)
+    def bisection_method(self, a, b, epsilon, pr=False):
+        if pr: print('-'*70 + '\n' + ' ' * 27 + 'Bisection method' + ' ' * 27 + '\n' + '-'*70)
         x = (a + b) / 2
         iters = 0
         while math.fabs(b-a) > epsilon or math.fabs(self.function(x)) > epsilon:
@@ -48,13 +48,13 @@ class Polynom:
                 a = x
             x = (a + b)/2
             iters += 1
-            print(iters, 'iteration, a =', round(a, 6), 'b =', round(b, 6), 'f(a) =', round(self.function(a), 6), 'f(b) =', round(self.function(b), 6))
-        print('Number of iterations:', iters)
+            if pr: print(iters, 'iteration, a =', round(a, 6), 'b =', round(b, 6), 'f(a) =', round(self.function(a), 6), 'f(b) =', round(self.function(b), 6))
+        if pr: print('Number of iterations:', iters)
 
         return x
 
-    def chord_method(self, a, b, epsilon):
-        print('-'*70 + '\n' + ' ' * 28 + 'Hordes method' + ' ' * 29 + '\n' + '-'*70)
+    def chord_method(self, a, b, epsilon, pr=False):
+        if pr: print('-'*70 + '\n' + ' ' * 28 + 'Hordes method' + ' ' * 29 + '\n' + '-'*70)
         f_a = self.function(a)
         f_b = self.function(b)
         x_prev = 1000000
@@ -72,13 +72,13 @@ class Polynom:
             x = (a * f_b - b * f_a) / (f_b - f_a)
             f_x = self.function(x)
             iters += 1
-            print(iters, 'iteration, a =', round(a, 6), 'b =', round(b, 6), 'f(a) =', round(f_a, 6), 'f(b) = ', round(f_b, 6))
-        print('Number of iterations:', iters)
+            if pr: print(iters, 'iteration, a =', round(a, 6), 'b =', round(b, 6), 'f(a) =', round(f_a, 6), 'f(b) = ', round(f_b, 6))
+        if pr: print('Number of iterations:', iters)
 
         return x
 
-    def newtons_method(self, a, b, epsilon):
-        print('-'*70 + '\n' + ' ' * 26 + 'Newton\'s method' + ' ' * 27 + '\n' + '-'*70)
+    def newtons_method(self, a, b, epsilon, pr=False):
+        if pr: print('-'*70 + '\n' + ' ' * 26 + 'Newton\'s method' + ' ' * 27 + '\n' + '-'*70)
 
         if self.function(a) * self.sec_derivative(a) > 0:
             x = a
@@ -92,17 +92,17 @@ class Polynom:
             x -= dx
             dx = self.function(x) / self.derivative(x)
             iters += 1
-            print(iters, 'iteration, x =', x, 'f(x) =', self.function(x))
-        print('Number of iterations:', iters)
+            if pr: print(iters, 'iteration, x =', x, 'f(x) =', self.function(x))
+        if pr: print('Number of iterations:', iters)
 
         return x
 
 
 if __name__ == "__main__":
     var = Polynom(6, -4, 2, 1, -3, 2)
-    print('x =', round(var.bisection_method(-2, -1, 1e-5), 5))
-    print('x =', round(var.chord_method(-2, -1, 1e-5), 5))
-    print('x =', round(var.newtons_method(-2, -1, 1e-5), 5))
+    print('x =', round(var.bisection_method(-3, 0, 1e-5, pr=True), 5))
+    print('x =', round(var.chord_method(-3, -0, 1e-5, pr=True), 5))
+    print('x =', round(var.newtons_method(-3, 0, 1e-5, pr=True), 5))
 
 elif __name__ == "__init__":
     pass
